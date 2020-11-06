df8731cfa047ea380eb288226bb177c99beb2952
Carlos Castillo <carloseduardocp.01@gmail.com>
2020-11-06 17:35:35 -0300

    Delete config.sh

--- config.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-apt-get update
-apt-get install neovim -y
-mkdir ~/.config/nvim
-cd ~/.config
-git clone https://github.com/itscarlosecp/linux-setup.git
-mv ./linux-setup/nvim ./nvim
-rm -rf ./linux-setup
