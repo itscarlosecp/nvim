8a74ee48fb136f786e71bb1313508da6175d24fb
Carlos Castillo <carloseduardocp.01@gmail.com>
2020-12-05 00:20:02 -0300

    Don't remember what I changed

--- install.sh
+++ install.sh
@@ -32,7 +32,7 @@
 # Apt Installs
 sudo add-apt-repository ppa:obsproject/obs-studio
 sudo apt update
-sudo apt install neovim neofetch git kitty zsh gnome-tweaks obs-studio -y
+sudo apt install neovim neofetch git kitty zsh gnome-tweaks obs-studio postgresql postgresql-contrib vlc p7zip-full -y
 
 # Snap Installs
 sudo snap install slack --classic
@@ -53,8 +53,10 @@ rm 'Caskaydia Cove Regular Nerd Font Complete.otf'
 sudo update-alternatives --config x-terminal-emulator
 THEME=https://raw.githubusercontent.com/dexpota/kitty-themes/master/themes/gruvbox_dark.conf
 wget "$THEME" -P ~/.config/kitty/themes
-touch ~/.config/kitty/kitty.conf
-printf "include ./themes/gruvbox_dark.conf\n\nfont_family CaskaydiaCove Nerd Font\ncursor_shape beam\nbackground_opacity 1" > ~/.config/kitty/kitty.conf
+mv kitty.conf ~/.config/kitty
+
+# Default Folders Configuration
+mv user-dirs.dirs ~/.config
 
 # Neovim Configuration
 mkdir ~/.config/nvim/
@@ -64,39 +66,6 @@ mv nvim ~/.config
 sudo apt install curl -y
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-
-# Gnome Customization
-# Icon Theme
-wget https://dllb2.pling.com/api/files/download/j/eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6IjE2MDU3NTg1ODYiLCJ1IjpudWxsLCJsdCI6ImRvd25sb2FkIiwicyI6IjgxOWFmZGE0NzFiZDJhODBmYzcxNmM5YWQ2NGM3MWRkM2E2OTk1NGUxZjAzZTMwNzE5NzBkNWIwMmFkMDA1OTRiMTMwMmVhYzI4NjFhNTgwMzE0NmUxYWNjMGNiM2MwOWMzMzdmOWVkY2E2MTY0MDgzOTkwODMxZGM4NmE5YzliIiwidCI6MTYwNzExNjIzNiwic3RmcCI6ImZlNTcyNDk1MDNlOTEwYmQ2Njg0YTY1OWFmZWQwMjQxIiwic3RpcCI6IjIwMC4xMjYuMjQxLjEwNiJ9.o1D_bE2GSstIEEEG2zjMSPgWylBdfGFPaiqm0UFEtqE/WhiteSur.tar.xz
-mkdir white-sur-icons
-tar -xvf WhiteSur.tar.xz -C white-sur-icons
-rm -rf WhiteSur.tar.xz
-cd white-sur-icons
-mkdir ~/.icons
-mv WhiteSur WhiteSur-dark ~/.icons/
-cd ..
-rm -rf white-sur-icons
-
-# GTK and Shell Themes
-wget https://dllb2.pling.com/api/files/download/j/eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6IjE2MDQ1MjAyMzUiLCJ1IjpudWxsLCJsdCI6ImRvd25sb2FkIiwicyI6IjBhZTVmYTQ1ZmJlYTgwMjIwOTk4MjI4MmQ5MDIxZTVlMzE2YjkxNDY2ZDU3NzRkMWQ5MDQyYWI2YjIyYzcxNjdhOWU3YTdkMzJkN2JkNTIyODFhYjcwMjQ5NmEzNTA2YmU1YTRhMmI3Zjk2MmYzNjE4NzA1YmUwNjNiODc5YThiIiwidCI6MTYwNzExNDcwMCwic3RmcCI6ImZlNTcyNDk1MDNlOTEwYmQ2Njg0YTY1OWFmZWQwMjQxIiwic3RpcCI6IjIwMC4xMjYuMjQxLjEwNiJ9.JI24Fxe2PuAKXZnCjoH27eG00qxzaWZupxE2H-bh0is/Yaru-Teal-v20.10.1.tar.xz
-mkdir theme
-mkdir ~/.themes
-tar -xvf Yaru-Teal-v20.10.1.tar.xz -C theme 
-rm -rf Yaru-Teal-v20.10.1.tar.xz
-cd theme/Theme 
-mv Yaru-Teal-dark ~/.themes/
-cd ../../
-rm -rf theme 
-
-# Cursors
-wget https://dllb2.pling.com/api/files/download/j/eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6IjE1ODA0MDIzODkiLCJ1IjpudWxsLCJsdCI6ImRvd25sb2FkIiwicyI6ImVjMTRlNWRhODk1OGI3NzdlZDQ4ZDFhZTJlZDg4YWU1YmZlMjM5MzkxZmFmY2ViMTlhYWU2OTk5NzhjMGQ2NjdmMjk1ZjA1MjlhYzZkN2NhYTEzNTk1MGM0NmJlY2ViNmNhNjYxYWMyYTg0MGY1NDllN2YxYzNhYzAzOTllOWZmIiwidCI6MTYwNzExNzYzOSwic3RmcCI6ImZlNTcyNDk1MDNlOTEwYmQ2Njg0YTY1OWFmZWQwMjQxIiwic3RpcCI6IjIwMC4xMjYuMjQxLjEwNiJ9.h2cRzNJ84FsbDcv3QdFD7CZ9E859qcQM57Vy0DLeHfA/McMojave-cursors.tar.xz
-mkdir cursors
-tar -xvf McMojave-cursors.tar.xz -C cursors
-rm -rf McMojave-cursors.tar.xz
-cd cursors
-cp -r McMojave-cursors ~/.icons/
-cd ..
-rm -rf cursors
 
 # Libinput Gestures and Gestures GUI
 sudo gpasswd -a $USER input
@@ -122,6 +91,11 @@ sudo apt install node-typescript make git -y
 git clone https://github.com/pop-os/shell
 cd shell
 make local-install
+
+# Lotion (Notion) Installation
+cd ~
+git clone https://github.com/puneetsl/lotion
+sudo ./install_native.sh
 
 # Oh My Zsh Configuration
 sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
