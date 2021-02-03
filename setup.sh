686bdff7633c30c3b2029d4c30ecd8c93e1a6dd5
Carlos Castillo <47466248+itscarlosecp@users.noreply.github.com>
2021-02-03 13:36:03 -0300

    Added JetBrainsMono download link to setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
+wget https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/JetBrainsMono/Regular/complete/JetBrains%20Mono%20Regular%20Nerd%20Font%20Complete.ttf
+
 ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/.dotfiles/nvim ~/.config/nvim
 ln -s ~/.dotfiles/alacritty ~/.config/alacritty
 ln -s ~/.dotfiles/.tmux.conf ~/.tmux.conf
-test
