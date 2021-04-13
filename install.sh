70c42e265e3e6c3106d1a1303b7c0ccdbbde5bda
Carlos Castillo <47466248+itscarlosecp@users.noreply.github.com>
2021-04-13 19:24:41 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -23,26 +23,20 @@ if ! grep -q "deb .*mmstick76/alacritty" /etc/apt/sources.list /etc/apt/sources.
 	sudo add-apt-repository ppa:mmstick76/alacritty -y
 fi
 sudo apt install alacritty neovim tmux vlc git curl ripgrep libinput-tools wmctrl xdotool make neofetch gnome-tweaks cmatrix sl cowsay -y
-sudo snap install spotify discord
+sudo snap install spotify
 
-#Download Config Files
-if [ ! -d "~/.dotfiles" ]; then
-	ln -sf ./.gitconfig ~/.gitconfig
-	ln -sf ./nvim ~/.config/nvim
-	ln -sf ./alacritty ~/.config/alacritty
-	ln -sf ./.tmux.conf ~/.tmux.conf
-	ln -sf ./.libinput-gestures.conf ~/.config/libinput-gestures.conf
-fi
+#Create Symlinks
+ln -sf ~/.dotfiles/nvim ~/.config/nvim
+ln -sf ~/.dotfiles/alacritty ~/.config/alacritty
+ln -sf ~/.dotfiles/.tmux.conf ~/.tmux.conf
+ln -sf ~/.dotfiles/libinput-gestures.conf ~/.config/libinput-gestures.conf
 
 #Install Neovim Plugins
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
 	https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim' && nvim +slient +PlugInstall +qall
 
 #Install Node
-wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash &&
+wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
 
 #Get Font
 wget https://raw.githubusercontent.com/ryanoasis/nerd-fonts/master/patched-fonts/JetBrainsMono/Ligatures/Regular/complete/JetBrains%20Mono%20Regular%20Nerd%20Font%20Complete.ttf -P ~/.fonts
-
-#Libinput Gestures
-git clone https://github.com/bulletmark/libinput-gestures.git ~/libinput-gestures && sudo make install && libinput-gestures-setup autostart && libinput-gestures-setup start
