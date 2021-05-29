0c990ebe66b1b83b4b209f5d45a5fdcc06e26183
Carlos Castillo <47466248+itscarlosecp@users.noreply.github.com>
2021-05-29 14:49:06 -0300

    Changed dev.sh install scripts to pacman

--- utils/dev.sh
+++ utils/dev.sh
@@ -1,21 +1,19 @@
 #!bin/sh
 export CONDA_DOWNLOAD_URL="https://repo.anaconda.com/archive/Anaconda3-2020.11-Linux-x86_64.sh"
-export GO_DOWNLOAD_URL="https://golang.org/dl/go1.16.3.linux-amd64.tar.gz"
-export JULIA_DOWNLOAD_URL="https://julialang-s3.julialang.org/bin/linux/x64/1.6/julia-1.6.1-linux-x86_64.tar.gz"
-export RUST_DOWNLOAD_URL="https://sh.rustup.rs"
 
 # Conda
 wget $CONDA_DOWNLOAD_URL | bash
 
 # Go
-sudo rm -rf /usr/local/go
-wget -O- $GO_DOWNLOAD_URL | sudo tar -xzf - -C /usr/local
+sudo add-apt-repository ppa:longsleep/golang-backports
+sudo apt update
+sudo apt install golang-go
 
 # Julia
-wget $JULIA_DOWNLOAD_URL
+sudo apt-get install julia
 
 # Rust
-curl --proto '=https' --tlsv1.2 -sSf $RUST_DOWNLOAD_URL | sh
+sudo apt install rustc
 
 # Haskell
 sudo apt-get install haskell-platform
