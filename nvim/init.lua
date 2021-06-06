c6449dcf414e8b03274ea0aa085e51bf890b5f56
Carlos Castillo <47466248+itscarlosecp@users.noreply.github.com>
2021-06-05 21:50:47 -0300

    Starting over

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,13 +1,2 @@
-require"vimscript"
-require"plugins"
-
-require"lv-fterm"
-require"lv-trouble"
-require"lv-treesitter"
-require"lv-colorizer"
-require"lv-galaxyline"
-require"lv-compe"
-
--- require"lsp"
-require"lspconfig"
-require"lsp.lua-ls"
+require"module._settings"
+require"moduel._mappings"
