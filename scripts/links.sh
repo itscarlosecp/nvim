5f8eb50df621ee154b625fe1a4c76a58ade8af23
Carlos Castillo <47466248+itscarlosecp@users.noreply.github.com>
2021-06-07 23:03:07 -0300

    Starting creting install scripts

--- scripts/links.sh
+++ scripts/links.sh
@@ -3,9 +3,9 @@
 #Create Symlinks
 ln -sf $HOME/.dotfiles/nvim ~/.config/nvim
 ln -sf $HOME/.dotfiles/alacritty ~/.config/alacritty
-ln -sf $HOME/.dotfiles/.tmux.conf ~/.tmux.conf
+ln -sf $HOME/.dotfiles/tmux/.tmux.conf ~/.tmux.conf
 ln -sf $HOME/.dotfiles/.bashrc ~/.bashrc
 ln -sf $HOME/.dotfiles/.zshrc ~/.zshrc
 ln -sf $HOME/.dotfiles/.gitconfig ~/.gitconfig
-ln -sf $HOME/.dotfiles/starship.toml ~/.config/starship.toml
+ln -sf $HOME/.dotfiles/starship/starship.toml ~/.config/starship.toml
 
---
+++ testing.sh
@@ -0,0 +1,4 @@
+#!bin/sh
+if [[ which go ]]; then
+	echo "go exists"
+fi
