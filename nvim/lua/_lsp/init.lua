8f5e1ef715bb54e57cd04acc9a7e11d9eeddeacf
Carlos Castillo <47466248+itscarlosecp@users.noreply.github.com>
2021-07-04 22:45:22 -0300

    Everything working

--- nvim/lua/_lsp/init.lua
+++ nvim/lua/_lsp/init.lua
@@ -29,19 +29,19 @@ vim.lsp.protocol.CompletionItemKind = {
 vim.fn.sign_define(
   "LspDiagnosticsSignError",
   {texthl = "LspDiagnosticsSignError", text = "", numhl = "LspDiagnosticsSignError"}
-)
+  )
 vim.fn.sign_define(
   "LspDiagnosticsSignWarning",
   {texthl = "LspDiagnosticsSignWarning", text = "", numhl = "LspDiagnosticsSignWarning"}
-)
+  )
 vim.fn.sign_define(
   "LspDiagnosticsSignHint",
   {texthl = "LspDiagnosticsSignHint", text = "", numhl = "LspDiagnosticsSignHint"}
-)
+  )
 vim.fn.sign_define(
   "LspDiagnosticsSignInformation",
   {texthl = "LspDiagnosticsSignInformation", text = "", numhl = "LspDiagnosticsSignInformation"}
-)
+  )
 
 local configs = {
   lua = {
