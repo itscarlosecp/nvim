f3a814b4662e78d88544938697140b6215be901e
Carlos <47466248+itscarlosecp@users.noreply.github.com>
2021-07-09 13:24:28 -0300

    Added kitty and awesome configs

--- scripts/links.sh
+++ scripts/links.sh
@@ -8,4 +8,6 @@ ln -sf $HOME/.dotfiles/lsd/config.yaml $HOME/.config/lsd
 # Applications
 ln -sTf $HOME/.dotfiles/nvim $HOME/.config/nvim
 ln -sf $HOME/.dotfiles/vim/.vimrc $HOME/.vimrc
+ln -sf $HOME/.dotfiles/kitty/kitty.conf $HOME/.config/kitty/kitty.conf
+ln -sf $HOME/.dotfiles/vim/.vimrc $HOME/.vimrc
 ln -sf $HOME/.dotfiles/starship/starship.toml $HOME/.config/starship.toml
